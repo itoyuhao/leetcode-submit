75ae89e7c38178ccf23e2ffef193ec6f2f09a74f
Yu-Hao Chen <83114630+itoyuhao@users.noreply.github.com>
2026-01-28 22:43:23 +0800

    [485] [Time Beats: 87.07%] [Memory Beats: 15.63%] - LeetPush

--- Arrays101/Max-Consecutive-Ones.py
+++ Arrays101/Max-Consecutive-Ones.py
@@ -1,19 +1,20 @@
-1class Solution:
-2    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-3        longest_record = 0
-4        current_consecutive = 0
-5        for i in nums:
-6            if i == 1:
-7                current_consecutive += 1
-8            else:
-9                if longest_record < current_consecutive:
-10                    longest_record = current_consecutive
-11                current_consecutive = 0
-12        
-13        if longest_record < current_consecutive:
-14            longest_record = current_consecutive
-15                    
-16        
-17        return longest_record
-18                    
-19                
+1# https://leetcode.com/problems/max-consecutive-ones/description/
+2class Solution:
+3    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+4        longest_record = 0
+5        current_consecutive = 0
+6        for i in nums:
+7            if i == 1:
+8                current_consecutive += 1
+9            else:
+10                if longest_record < current_consecutive:
+11                    longest_record = current_consecutive
+12                current_consecutive = 0
+13        
+14        if longest_record < current_consecutive:
+15            longest_record = current_consecutive
+16                    
+17        
+18        return longest_record
+19                    
+20                
