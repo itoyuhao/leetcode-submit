63cf472a390a44be0fa79674409ed0b7809ae0b8
Yu-Hao Chen <83114630+itoyuhao@users.noreply.github.com>
2026-01-28 22:42:17 +0800

    [1295] [Time Beats: 55.11%] [Memory Beats: 11.25%] - LeetPush

--- Arrays101/Find-Numbers-with-Even-Number-of-Digits.py
+++ Arrays101/Find-Numbers-with-Even-Number-of-Digits.py
@@ -1,18 +1,19 @@
-1class Solution:
-2    def findNumbers(self, nums: List[int]) -> int:
-3        res = 0
-4        for i in range(len(nums)):
-5            digits = self.digitCounter(nums[i])
-6            if digits % 2 == 0:
-7                res += 1
-8        
-9        return res
-10    
-11    @staticmethod
-12    def digitCounter(s: int):
-13        digit_cnt = 0
-14        while s > 0:
-15            digit_cnt += 1
-16            s //= 10
-17        
-18        return digit_cnt
+1# https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+2class Solution:
+3    def findNumbers(self, nums: List[int]) -> int:
+4        res = 0
+5        for i in range(len(nums)):
+6            digits = self.digitCounter(nums[i])
+7            if digits % 2 == 0:
+8                res += 1
+9        
+10        return res
+11    
+12    @staticmethod
+13    def digitCounter(s: int):
+14        digit_cnt = 0
+15        while s > 0:
+16            digit_cnt += 1
+17            s //= 10
+18        
+19        return digit_cnt
